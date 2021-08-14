d665fbe128ba4e590ccaed4fc0ca3b536d073d8e
ozkeeeeki <87401269+ozeeeeki@users.noreply.github.com>
2021-08-14 17:22:18 +0900

    add:加速度センサ読みだし処理を実装

--- src/main.cpp
+++ src/main.cpp
@@ -16,14 +16,17 @@ LSM9DS1 imu(&i2c);
 // main() runs in its own thread in the OS
 int main() {
     imu.start();
+    ThisThread::sleep_for(100ms);
+    if(imu.getStatus()==LSM9DS1_STATUS_SUCCESS_TO_CONNECT){
+        snprintf(printBuffer, PRINT_BUFFER_SIZE, "Succeeded connecting LSM9DS1.\r\n");
+        serial.write(printBuffer,strlen(printBuffer));
+    }else{
+        snprintf(printBuffer, PRINT_BUFFER_SIZE, "Failed to connect LSM9DS1.\r\n");
+        serial.write(printBuffer,strlen(printBuffer));
+    }
     while(true){
-        if(imu.getStatus()==LSM9DS1_STATUS_SUCCESS_TO_CONNECT){
-            snprintf(printBuffer, PRINT_BUFFER_SIZE, "Succeeded connecting LSM9DS1.\r\n");
-            serial.write(printBuffer,strlen(printBuffer));
-        }else{
-            snprintf(printBuffer, PRINT_BUFFER_SIZE, "Failed to connect LSM9DS1.\r\n");
-            serial.write(printBuffer,strlen(printBuffer));
-        }
+        snprintf(printBuffer, PRINT_BUFFER_SIZE, "$%f %f %f;\r\n",imu.accX(), imu.accY(), imu.accZ());
+        serial.write(printBuffer,strlen(printBuffer));            
         ThisThread::sleep_for(100ms);
     }
 }
