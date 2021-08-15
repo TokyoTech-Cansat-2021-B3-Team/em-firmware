254800680230022be897cdd8850859eb2832628c
k.hayashi <kazuki618@384.jp>
2021-08-15 20:58:47 +0900

    add: BME280クラス&動作確認

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,24 @@
 #include "mbed.h"
 
+#include "PinAssignment.h"
+
+#include "BME280.h"
+
+I2C i2c(I2C_SDA, I2C_SCL);
+
+BME280 bme280(&i2c);
+
 // main() runs in its own thread in the OS
-int main() {}
+int main() {
+  i2c.frequency(400000);
+
+  bme280.start();
+
+  while (true) {
+    printf("press: %lf Pa\n", bme280.getPressure());
+    printf("temp: %lf DegC\n", bme280.getTemprature());
+    printf("hum: %lf %%RH\n", bme280.getHumidity());
+
+    ThisThread::sleep_for(100ms);
+  }
+}
