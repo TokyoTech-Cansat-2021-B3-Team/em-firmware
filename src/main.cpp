24dfa0525fc80c9b442af394c14c44eaec56cef4
k.hayashi <kazuki618@384.jp>
2021-08-17 06:29:26 +0900

    add: 上下駆動用モータテストコード

--- src/main.cpp
+++ src/main.cpp
@@ -2,15 +2,20 @@
 
 #include "PinAssignment.h"
 
+#include "DCMotor.h"
 #include "DrillMotor.h"
 
-PwmOut motor4(M4_IN1);
+PwmOut motor3In1(M3_IN1);
+PwmOut motor3In2(M3_IN2);
+PwmOut motor4In1(M4_IN1);
 
-DrillMotor drillMotor(&motor4);
+DCMotor verticalMotor(&motor3In1, &motor3In2);
+DrillMotor drillMotor(&motor4In1);
 
 // main() runs in its own thread in the OS
 int main() {
   while (true) {
+    // ドリル用モータ
     drillMotor.forward(1.0);
 
     ThisThread::sleep_for(2s);
@@ -20,6 +25,32 @@ int main() {
     ThisThread::sleep_for(2s);
 
     drillMotor.stop();
+
+    ThisThread::sleep_for(2s);
+
+    // 上下駆動用モータ上昇
+    verticalMotor.forward(1.0);
+
+    ThisThread::sleep_for(2s);
+
+    verticalMotor.forward(0.5);
+
+    ThisThread::sleep_for(2s);
+
+    verticalMotor.stop();
+
+    ThisThread::sleep_for(2s);
+
+    // 上下駆動用モータ下降
+    verticalMotor.reverse(1.0);
+
+    ThisThread::sleep_for(2s);
+
+    verticalMotor.reverse(0.5);
+
+    ThisThread::sleep_for(2s);
+
+    verticalMotor.stop();
 
     ThisThread::sleep_for(2s);
   }
