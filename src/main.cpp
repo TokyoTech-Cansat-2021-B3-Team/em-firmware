fec2ede324b2213c21bab59057ea3445c78273e4
ozkeeeeki <87401269+ozeeeeki@users.noreply.github.com>
2021-08-17 23:13:57 +0900

    modify:実装方針を変更＆mainを追加

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,16 @@
 
 #include "PinAssignment.h"
 
-#include "DCMotor.h"
-#include "DrillMotor.h"
 #include "QEI.h"
+
 #include "WheelMotor.h"
+#include "WheelPID.h"
+
+#define SPEED_TASK_PERIOD 10ms
+#define PRINT_BUFFER_SIZE 128
+
+char printBuffer[PRINT_BUFFER_SIZE];
+BufferedSerial serial(USBTX, USBRX, 115200);
 
 PwmOut motor1In1(M1_IN1);
 PwmOut motor1In2(M1_IN2);
@@ -13,79 +19,67 @@ PwmOut motor1In2(M1_IN2);
 PwmOut motor2In1(M2_IN1);
 PwmOut motor2In2(M2_IN2);
 
-PwmOut motor3In1(M3_IN1);
-PwmOut motor3In2(M3_IN2);
-
-PwmOut motor4In1(M4_IN1);
-
 WheelMotor leftWheelMotor(&motor1In1, &motor1In2);
 WheelMotor rightWheelMotor(&motor2In1, &motor2In2);
 
-DCMotor verticalMotor(&motor3In1, &motor3In2);
-DrillMotor drillMotor(&motor4In1);
-QEI verticalEncoder(ENC3_A, NC, NC, 6, QEI::CHANNEL_A_ENCODING);
+QEI leftEncoder(ENC1_A, NC, NC, 6, QEI::CHANNEL_A_ENCODING);
+QEI rightEncoder(ENC2_A, NC, NC, 6, QEI::CHANNEL_A_ENCODING);
 
-Thread encoderThread(osPriorityAboveNormal, 1024, nullptr, nullptr);
+Thread speedThread(osPriorityAboveNormal, 1024, nullptr, nullptr);
+Thread printThread(osPriorityAboveNormal, 1024, nullptr, nullptr);
 
-int previousRev;
+WheelPID leftPID;
+WheelPID rightPID;
 
-void encoderThreadLoop() {
+double leftSpeed = 0.0;
+double rightSpeed = 0.0;
+
+double pulsesToRpm(int pulses, chrono::microseconds period, double gear_ratio) {
+  // エンコーダー：6パルス/回転
+  return (double)abs(pulses) * 60.0 / 6.0 / gear_ratio / chrono::duration<double>(period).count(); // RPM
+}
+
+void speedThreadLoop() {
   while (true) {
-    printf("pulses: %d, rev: %d, rpm: %d\n", verticalEncoder.getPulses(), verticalEncoder.getRevolutions(),
-           (verticalEncoder.getRevolutions() - previousRev) * 60 * 10);
-
-    previousRev = verticalEncoder.getRevolutions();
-
-    ThisThread::sleep_for(100ms);
+      leftSpeed = pulsesToRpm(leftEncoder.getPulses(), SPEED_TASK_PERIOD, 1000.0);
+      leftEncoder.reset();
+      rightSpeed = pulsesToRpm(rightEncoder.getPulses(), SPEED_TASK_PERIOD, 249.8);
+      rightEncoder.reset();
+      leftPID.updatePIDOutput(leftSpeed, SPEED_TASK_PERIOD);
+      rightPID.updatePIDOutput(rightSpeed, SPEED_TASK_PERIOD);
+      //leftWheelMotor.forward(leftPID.getOutput());
+      //rightWheelMotor.forward(rightPID.getOutput());
+      motor1In1 = leftPID.getOutput();
+      motor2In1 = rightPID.getOutput();
+      rtos::ThisThread::sleep_for(SPEED_TASK_PERIOD);
   }
+}
+
+void printThreadLoop(){
+    while(true){
+        snprintf(printBuffer, PRINT_BUFFER_SIZE, "$L:%f R:%f ;\r\n",leftSpeed,rightSpeed);
+        serial.write(printBuffer,strlen(printBuffer));
+        ThisThread::sleep_for(100ms);
+    }
 }
 
 // main() runs in its own thread in the OS
 int main() {
-
-  encoderThread.start(encoderThreadLoop);
-
+  leftPID.setTargetSpeed(20);
+  rightPID.setTargetSpeed(20);
+  speedThread.start(speedThreadLoop);
+  printThread.start(printThreadLoop);
+  int i = 0;
+  int j = 0;
+  double output[3] = {20.0, 10.0, 0.0};
   while (true) {
-    // 上昇
-    leftWheelMotor.forward(1.0);
-    rightWheelMotor.forward(1.0);
-    drillMotor.forward(1.0);
-    verticalMotor.forward(1.0);
-
-    ThisThread::sleep_for(2s);
-
-    leftWheelMotor.forward(0.5);
-    rightWheelMotor.forward(0.5);
-    drillMotor.forward(0.5);
-    verticalMotor.forward(0.5);
-
-    ThisThread::sleep_for(2s);
-
-    leftWheelMotor.stop();
-    rightWheelMotor.stop();
-    drillMotor.stop();
-    verticalMotor.stop();
-
-    ThisThread::sleep_for(2s);
-
-    // 下降
-    leftWheelMotor.reverse(1.0);
-    rightWheelMotor.reverse(1.0);
-    verticalMotor.reverse(1.0);
-
-    ThisThread::sleep_for(2s);
-
-    leftWheelMotor.reverse(0.5);
-    rightWheelMotor.reverse(0.5);
-    verticalMotor.reverse(0.5);
-
-    ThisThread::sleep_for(2s);
-
-    leftWheelMotor.stop();
-    rightWheelMotor.stop();
-    drillMotor.stop();
-    verticalMotor.stop();
-
-    ThisThread::sleep_for(2s);
+    i++;
+    if(i*100 > 5000){
+        i = 0;
+        j++;
+        leftPID.setTargetSpeed(output[j%3]);
+        rightPID.setTargetSpeed(output[j%3]);
+    }
+    ThisThread::sleep_for(100ms);
   }
 }
--- src/middlewares/wheelPID.h
+++ src/middlewares/wheelPID.h
@@ -3,35 +3,14 @@
 #include "mbed.h"
 #include <cstdint>
 
-#include "WheelMotor.h"
-#include "QEI.h"
-
-#define WHEELPID_THREAD_PRIORITY osPriorityHigh
-#define WHEELPID_THREAD_STACK_SIZE 1024
-#define WHEELPID_THREAD_NAME "WHEELPID"
-
-#define WHEELPID_PERIOD 200ms
-#define WHEELPID_READ_SIZE 255
-
-enum DIRECTION{
-    CW,
-    CCW
-};
-
 class WheelPID{
 public:
-    explicit WheelPID(WheelMotor* wheelmotor, QEI* qei);
+    explicit WheelPID();
     void setTargetSpeed(double speed);
-    void setGearRatio(double gearratio);
-    void setDirection(DIRECTION direction);
+    void updatePIDOutput(double sensorSpeed, chrono::microseconds period);
+    double getOutput();
 private:
-    void start();
-    void stop();
-    void threadLoop();
-    void updatePIDOutput();
     void updateSensorSpeed();
-    double pulsesToRpm(int pulses);
-    DIRECTION _direction = CW;
     double _targetSpeed = 0.0;
     double _sensorSpeed = 0.0;
     double _previousSpeed = 0.0;
@@ -41,8 +20,4 @@ private:
     double _pGain = 0.01;
     double _iGain = 0.05;
     double _dGain = 0.02;
-    double _gearRatio = 249.8;
-    WheelMotor* _wheelmotor;
-    QEI* _qei;
-    unique_ptr<Thread> _thread;
 };
