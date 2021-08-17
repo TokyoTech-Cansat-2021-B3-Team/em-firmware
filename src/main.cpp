6e81211fc0a350295ebf72ec0e94bae262754a01
k.hayashi <kazuki618@384.jp>
2021-08-18 02:14:40 +0900

    fix: モータノイズが原因と考えられるためHWで対処
    ・とりあえず、
    モータにコンデンサ追加
    電源にコンデンサ追加
    配線の調整
    で発生確率を下げることに成功
    ・ある程度ハングする可能性はあるので、ウォッチドッグに期待するしかない

--- src/main.cpp
+++ src/main.cpp
@@ -7,11 +7,25 @@ PwmOut motor2In1(M2_IN1);
 
 // main() runs in its own thread in the OS
 int main() {
-  while (true) {
-    printf("Hello\n");
 
-    motor1In1 = 0.01;
-    motor2In1 = 0.01;
+  while (true) {
+    for (int i = 0; i < 1000; i++) {
+      printf("%d\n", i);
+
+      motor1In1 = i * 0.001;
+      motor2In1 = i * 0.001;
+
+      ThisThread::sleep_for(10ms);
+    }
+
+    for (int i = 1000; i >= 0; i--) {
+      printf("%d\n", i);
+
+      motor1In1 = i * 0.001;
+      motor2In1 = i * 0.001;
+
+      ThisThread::sleep_for(10ms);
+    }
 
     ThisThread::sleep_for(100ms);
   }
