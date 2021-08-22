96c6254c42a131a65a55f6da2f9dfd3bc65fdc08
k.hayashi <kazuki618@384.jp>
2021-08-23 06:05:20 +0900

    modify: main&mbed_app.json調整

--- src/main.cpp
+++ src/main.cpp
@@ -5,26 +5,22 @@
 
 #include "PinAssignment.h"
 
-#include "Logger.h"
+#include "MU2.h"
 #include "PA1010D.h"
 
-I2C i2c(I2C_SDA, I2C_SCL);
+#include "Console.h"
+#include "Logger.h"
 
+I2C i2c(I2C_SDA, I2C_SCL);
 PA1010D pa1010d(&i2c);
 
 SDBlockDevice sdBlockDevice(SPI_MOSI, SPI_MISO, SPI_SCLK, SPI_SSEL, 25000000);
-
 LittleFileSystem2 littleFileSystem2(nullptr);
 
-Logger logger(&sdBlockDevice, &littleFileSystem2);
-
-#include "Console.h"
-#include "MU2.h"
-
 BufferedSerial bufferedSerial(UART_TX, UART_RX, MU2_SERIAL_BAUDRATE);
-
 MU2 mu2(&bufferedSerial);
 
+Logger logger(&sdBlockDevice, &littleFileSystem2);
 Console console(&mu2);
 
 // main() runs in its own thread in the OS
@@ -32,7 +28,8 @@ int main() {
   console.init();
 
   while (true) {
-    console.lprintf("main", "downlink message\n");
+    console.lprintf("main", "log and downlink message\n");
 
     ThisThread::sleep_for(1s);
   }
+}
