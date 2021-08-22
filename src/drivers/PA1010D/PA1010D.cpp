f7d02ce24bfa65ce368ebdaa75a77fb02b2a11a4
k.hayashi <kazuki618@384.jp>
2021-08-23 06:59:31 +0900

    add: 多重start防止

--- src/drivers/PA1010D/PA1010D.cpp
+++ src/drivers/PA1010D/PA1010D.cpp
@@ -4,16 +4,20 @@ PA1010D::PA1010D(I2C *i2c)
     : _i2c(i2c), _thread(), _packetBuffer(), _packetBufferPosition(0), _rmc(), _gga(), _isSetTime(false) {}
 
 void PA1010D::start() {
-  _thread = make_unique<Thread>(PA1010D_THREAD_PRIORITY,   //
-                                PA1010D_THREAD_STACK_SIZE, //
-                                nullptr,                   //
-                                PA1010D_THREAD_NAME);
-  _thread->start(callback(this, &PA1010D::threadLoop));
+  if (!_thread) {
+    _thread = make_unique<Thread>(PA1010D_THREAD_PRIORITY,   //
+                                  PA1010D_THREAD_STACK_SIZE, //
+                                  nullptr,                   //
+                                  PA1010D_THREAD_NAME);
+    _thread->start(callback(this, &PA1010D::threadLoop));
+  }
 }
 
 void PA1010D::stop() {
-  _thread->terminate();
-  _thread.reset();
+  if (_thread) {
+    _thread->terminate();
+    _thread.reset();
+  }
 }
 
 void PA1010D::threadLoop() {
