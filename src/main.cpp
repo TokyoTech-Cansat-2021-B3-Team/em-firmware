246ed4c94d3406888deada9ba7dec4f4badf4cd0
k.hayashi <kazuki618@384.jp>
2021-08-26 05:51:21 +0900

    add: パラシュート分離シーケンス

--- src/main.cpp
+++ src/main.cpp
@@ -8,14 +8,15 @@
 #include "SDBlockDevice.h"
 
 // drivers
+#include "Fusing.h"
 #include "MU2.h"
-#include "PA1010D.h"
 
 // middlewares
 #include "Console.h"
 #include "Logger.h"
 
 // sequences
+#include "FusingSequence.h"
 
 // defines
 #define SPI_FREQUENCY 25000000
@@ -24,6 +25,8 @@
 // objects
 
 // embedded
+PwmOut fuseGate(FUSE_GATE);
+
 I2C i2c(I2C_SDA, I2C_SCL);
 BufferedSerial bufferedSerial(UART_TX, UART_RX, MU2_SERIAL_BAUDRATE);
 SDBlockDevice sdBlockDevice(SPI_MOSI, SPI_MISO, SPI_SCLK, SPI_SSEL, SPI_FREQUENCY);
@@ -31,18 +34,38 @@ LittleFileSystem2 littleFileSystem2(nullptr);
 
 // drivers
 MU2 mu2(&bufferedSerial);
-PA1010D pa1010d(&i2c);
+Fusing fusing(&fuseGate);
 
 // middlewares
 Logger logger(&sdBlockDevice, &littleFileSystem2);
 Console console(&mu2, &logger);
 
 // sequences
+FusingSequence fusingSequence(&fusing, &console);
+
+// パラシュート分離シーケンス
+void fusingSequenceSyncStart() {
+  fusingSequence.start();
+
+  console.log("main", "Fusing Sequence Sync Start");
+
+  // 正常終了
+  // フィードバックなしなので必ず起こる
+  while (fusingSequence.state() != FusingSequence::Complete) {
+    ThisThread::sleep_for(1s);
+  }
+
+  console.log("main", "Fusing Sequence Complete");
+
+  fusingSequence.stop();
+}
 
 // main() runs in its own thread in the OS
 int main() {
   // I2C速度変更
   i2c.frequency(I2C_FREQUENCY);
+
+  fusingSequenceSyncStart();
 
   while (true) {
     ThisThread::sleep_for(1s);
---
+++ src/sequences/FusingSequence/FusingSequence.cpp
@@ -0,0 +1,51 @@
+#include "FusingSequence.h"
+#include <chrono>
+
+FusingSequence::FusingSequence(Fusing *fusing, Console *console)
+    : _thread(),         //
+      _fusing(fusing),   //
+      _console(console), //
+      _isStart(false)    //
+{}
+
+void FusingSequence::threadLoop() {
+  // シーケンス開始
+  _state = Running;
+  _console->log("Fusing", "start Fusing Sequence\n");
+
+  _console->log("Fusing", "start heating %llu seconds\n",
+                chrono::duration_cast<chrono::seconds>(FUSING_SEQUENCE_HEAT_DURATION).count());
+
+  _fusing->heat(FUSING_SEQUENCE_HEAT_DURATION);
+
+  _console->log("Fusing", "stop heating\n");
+
+  // シーケンス終了
+  _state = Complete;
+  _console->log("Fusing", "Fusing Sequence complete\n");
+}
+
+void FusingSequence::start() {
+  if (!_isStart) {
+    _thread = make_unique<Thread>(FUSING_SEQUENCE_THREAD_PRIORITY,   //
+                                  FUSING_SEQUENCE_THREAD_STACK_SIZE, //
+                                  nullptr,                           //
+                                  FUSING_SEQUENCE_THREAD_NAME);
+    _thread->start(callback(this, &FusingSequence::threadLoop));
+
+    _isStart = true;
+  }
+}
+
+void FusingSequence::stop() {
+  if (_isStart) {
+    _thread->terminate();
+    _thread.reset();
+
+    _isStart = false;
+  }
+}
+
+FusingSequence::FusingSequenceState FusingSequence::state() {
+  return _state;
+}
