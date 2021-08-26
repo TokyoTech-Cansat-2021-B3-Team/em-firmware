dea276cabb60610f9f3933599741aaf0d9e9603e
k.hayashi <kazuki618@384.jp>
2021-08-26 10:55:49 +0900

    modify: リファクタリング&State導入

--- src/sequences/ProbeSequence/ProbeSequence.cpp
+++ src/sequences/ProbeSequence/ProbeSequence.cpp
@@ -1,101 +1,130 @@
 #include "ProbeSequence.h"
-#include <cstdio>
 
 ProbeSequence::ProbeSequence(DrillMotor *drillMotor, DCMotor *verticalMotor, Stepper *loadingMotor,
                              QEI *verticalEncoder)
-    : _thread(), _drillMotor(drillMotor), //
-      _verticalMotor(verticalMotor),      //
-      _loadingMotor(loadingMotor),        //
-      _probeNumber(Probe1),               //
-      _verticalEncoder(verticalEncoder)   //
+    : _thread(),                         //
+      _drillMotor(drillMotor),           //
+      _verticalMotor(verticalMotor),     //
+      _loadingMotor(loadingMotor),       //
+      _probeNumber(Probe1),              //
+      _verticalEncoder(verticalEncoder), //
+      _isStart(false),                   //
+      _state(Stop)                       //
 {}
 
 void ProbeSequence::threadLoop() {
   // startが呼ばれるとここから始まる
   // ここにシーケンスを書く
 
+  // シーケンス開始
+  _state = Running;
+
+  // 1本目のみ初期位置への移動
   if (_probeNumber == Probe1) {
-    set(20, 0.8, 0.3);
-    //変位、ストローク、デューティー比
-
-    //ホルダー回転角度、回転速さ(rps)、ストローク、電極接続長さ、刺しこみ長さ
-    //デューティー比(接続、ドリル)、デューティー比(接続、上下)、デューティー比(刺しこみ、ドリル)、デューティー比(刺しこみ、上下)
-    //デューティー比(初期位置移動、上下)
-    drilling(72.0, 60.0, 0.125, 0.8, 15, 30, 0.5, 0.5, 1.0, 0.3, 1.0);
+    verticalMove(PROBE_SEQUENCE_SETUP_VERTICAL_DUTY, PROBE_SEQUENCE_SETUP_LENGTH);
   }
 
-  // 2～4本
-  else {
-    //ホルダー回転角度、回転速さ(rps)、ストローク、電極接続長さ、刺しこみ長さ
-    //デューティー比(接続、ドリル)、デューティー比(接続、上下)、デューティー比(刺しこみ、ドリル)、デューティー比(刺しこみ、上下)
-    //デューティー比(初期位置移動、上下)
-    drilling(72.0, 60.0, 0.125, 0.8, 15, 30, 0.5, 0.5, 1.0, 0.3, 1.0);
-  }
+  // ホルダーの回転
+  holderToNext();
+
+  // 電極接続
+  connect();
+
+  // 刺し込み
+  drilling();
+
+  // 初期位置へ戻る
+  back();
+
+  // シーケンス終了
+  _state = Complete;
 }
 
-void ProbeSequence::set(double L, double stroke, double duty) {
-  //上下駆動用モーターを回す
-  _verticalMotor->reverse(duty);
-  //エンコーダーでセンシング
-  _verticalEncoder->reset();
-  int i = 0;
-  int loop = L / stroke * 6 * 249;
-  while (i < loop) {
-    i = _verticalEncoder->getPulses();
-  }
-  //モーター停止
-  _verticalMotor->stop();
+void ProbeSequence::holderToNext() {
+  // 1段階目
+  _loadingMotor->rotate(PROBE_SEQUENCE_HOLDER_ANGLE_FIRST, PROBE_SEQUENCE_HOLDER_SPEED);
+
+  // 待ち
+  ThisThread::sleep_for(PROBE_SEQUENCE_HOLDER_DELAY);
+
+  // 2段目
+  _loadingMotor->rotate(PROBE_SEQUENCE_HOLDER_ANGLE_SECOND, PROBE_SEQUENCE_HOLDER_SPEED);
 }
 
-//刺しこみ一連の動作
-void ProbeSequence::drilling(double angle, double angle_0, double speed, double stroke, double dl, double L,
-                             double dutyA_connect, double dutyB_connect, double dutyA_drilling, double dutyB_drilling,
-                             double duty_back) {
+void ProbeSequence::connect() {
+  // ドリル回転開始
+  _drillMotor->forward(PROBE_SEQUENCE_CONNECT_DRILL_DUTY);
 
-  //～ホルダー回転～
-  _loadingMotor->rotate((angle - angle_0), speed);
-  ThisThread::sleep_for(2s);
-  _loadingMotor->rotate(angle_0, speed);
+  // 上下駆動指定量下降
+  verticalMove(PROBE_SEQUENCE_CONNECT_VERTICAL_DUTY, //
+               PROBE_SEQUENCE_CONNECT_LENGTH);
 
-  //～電極接続～
-  s(dutyA_connect, dutyB_connect, stroke, dl);
-
-  //～刺しこみ～
-  s(dutyA_drilling, dutyB_drilling, stroke, L);
-
-  //～初期位置に戻す～
-  set(dl + L, stroke, duty_back);
-}
-
-void ProbeSequence::s(double pwmA, double pwmB, double stroke, double L) {
-  //モーター回転
-  _drillMotor->forward(pwmA);
-  _verticalMotor->forward(pwmB);
-
-  //エンコーダーでセンシング
-  _verticalEncoder->reset();
-  int i = 0;
-  int loop = L / stroke * 6 * 249;
-  while (i < loop) {
-    i = _verticalEncoder->getPulses();
-  }
-
-  //モーター停止
-  _verticalMotor->stop();
+  // ドリル停止
   _drillMotor->stop();
 }
 
-void ProbeSequence::start(ProbeNumber probeNumber) {
-  _probeNumber = probeNumber;
+void ProbeSequence::drilling() {
+  // ドリル回転開始
+  _drillMotor->forward(PROBE_SEQUENCE_DRILLING_DRILL_DUTY);
 
-  _thread = make_unique<Thread>(THREAD_PATTERN_THREAD_PRIORITY,   //
-                                THREAD_PATTERN_THREAD_STACK_SIZE, //
-                                nullptr,                          //
-                                THREAD_PATTERN_THREAD_NAME);
-  _thread->start(callback(this, &ProbeSequence::threadLoop));
+  // 上下駆動指定量下降
+  verticalMove(PROBE_SEQUENCE_DRILLING_VERTICAL_DUTY, //
+               PROBE_SEQUENCE_DRILLING_LENGTH);
+
+  // ドリル停止
+  _drillMotor->stop();
+}
+
+void ProbeSequence::back() {
+  // 上下駆動
+  // 電極接続 + 刺し込み の下降分だけ上昇
+  verticalMove(PROBE_SEQUENCE_BACK_VERTIVAL_DUTY, //
+               -(PROBE_SEQUENCE_CONNECT_LENGTH + PROBE_SEQUENCE_DRILLING_LENGTH));
+}
+
+void ProbeSequence::verticalMove(double duty, double L) {
+  _verticalEncoder->reset();
+
+  if (L >= 0.0) {
+    _verticalMotor->forward(duty);
+  } else {
+    _verticalMotor->reverse(duty);
+  }
+
+  while (revToLength(_verticalEncoder->getRevolutions()) < L) {
+    ThisThread::sleep_for(10ms);
+  }
+
+  _verticalMotor->stop();
+}
+
+double ProbeSequence::revToLength(int revolution) {
+  return revolution * PROBE_SEQUENCE_VERTICAL_RATIO * PROBE_SEQUENCE_VERTICAL_LEAD;
+}
+
+void ProbeSequence::start(ProbeNumber probeNumber) {
+  if (!_isStart) {
+    _probeNumber = probeNumber;
+
+    _thread = make_unique<Thread>(THREAD_PATTERN_THREAD_PRIORITY,   //
+                                  THREAD_PATTERN_THREAD_STACK_SIZE, //
+                                  nullptr,                          //
+                                  THREAD_PATTERN_THREAD_NAME);
+    _thread->start(callback(this, &ProbeSequence::threadLoop));
+
+    _isStart = true;
+  }
 }
 
 void ProbeSequence::stop() {
-  _thread->terminate();
-  _thread.reset();
+  if (_isStart) {
+    _thread->terminate();
+    _thread.reset();
+
+    _isStart = false;
+  }
+}
+
+ProbeSequenceState ProbeSequence::state() {
+  return _state;
 }
--- src/sequences/ProbeSequence/ProbeSequence.h
+++ src/sequences/ProbeSequence/ProbeSequence.h
@@ -7,9 +7,37 @@
 #include "QEI.h"
 #include "Stepper.h"
 
-#define THREAD_PATTERN_THREAD_PRIORITY osPriorityNormal
-#define THREAD_PATTERN_THREAD_STACK_SIZE 1024
-#define THREAD_PATTERN_THREAD_NAME "ProbeSequence"
+#define PROBE_SEQUENCE_THREAD_PRIORITY osPriorityNormal
+#define PROBE_SEQUENCE_THREAD_STACK_SIZE 2048
+#define PROBE_SEQUENCE_THREAD_NAME "ProbeSequence"
+
+// パラメータ
+#define PROBE_SEQUENCE_VERTICAL_LEAD 0.8     // 上下駆動のねじリード 0.8 mm
+#define PROBE_SEQUENCE_VERTICAL_RATIO 249    // 上下駆動のギア比 1:249
+#define PROBE_SEQUENCE_VERTICAL_POLLING 10ms // エンコーダの値の確認周期
+
+// 1本目刺し込み前の初期位置への移動
+#define PROBE_SEQUENCE_SETUP_VERTICAL_DUTY 0.3 // 上下Duty
+#define PROBE_SEQUENCE_SETUP_LENGTH -20        // 上下駆動下降量 (mm)
+
+// ホルダー回転
+#define PROBE_SEQUENCE_HOLDER_ANGLE_FIRST 12.0  // 1段階目の回転角
+#define PROBE_SEQUENCE_HOLDER_ANGLE_SECOND 60.0 // 2段階目の回転角
+#define PROBE_SEQUENCE_HOLDER_DELAY 2s          // 1段階目と2段階目の間の時間
+#define PROBE_SEQUENCE_HOLDER_SPEED 0.125       // 回転速度 (rps)
+
+// 電極接続
+#define PROBE_SEQUENCE_CONNECT_DRILL_DUTY 0.5    // ドリルDuty
+#define PROBE_SEQUENCE_CONNECT_VERTICAL_DUTY 0.5 // 上下Duty
+#define PROBE_SEQUENCE_CONNECT_LENGTH 15         // 上下駆動下降量 (mm)
+
+// 刺し込み
+#define PROBE_SEQUENCE_DRILLING_DRILL_DUTY 1.0    // ドリルDuty
+#define PROBE_SEQUENCE_DRILLING_VERTICAL_DUTY 0.3 // 上下Duty
+#define PROBE_SEQUENCE_DRILLING_LENGTH 30         // 上下駆動下降量 (mm)
+
+// 初期位置に戻る
+#define PROBE_SEQUENCE_BACK_VERTIVAL_DUTY 1.0 // 上下Duty
 
 class ProbeSequence {
 private:
@@ -20,6 +48,13 @@ public:
     Probe2 = 2,
     Probe3 = 3,
     Probe4 = 4,
+  };
+
+  using ProbeSequenceState = enum {
+    Running,
+    Complete,
+    SequenceTimeout,
+    Stop,
   };
 
 private:
@@ -33,16 +68,34 @@ private:
   // 電極の番号をThreadに通知
   ProbeNumber _probeNumber;
 
+  ProbeSequenceState _state;
+
+  bool _isStart;
+
 public:
 private:
   void threadLoop();
 
   void set(double L, double stroke, double duty);
 
-  void drilling(double angle, double angle_0, double speed, double stroke, double dl, double L, double dutyA_connect,
-                double dutyB_connect, double dutyA_drilling, double dutyB_drilling, double duty_back);
+  // ホルダーの回転
+  void holderToNext();
 
-  void s(double pwmA, double pwmB, double stroke, double L);
+  // 電極の接続
+  void connect();
+
+  // 刺し込み
+  void drilling();
+
+  // 初期位置に戻る
+  void back();
+
+  // 指定した距離だけ、上下駆動する
+  // strokeは下降する向きを正、上昇する向きを負
+  void verticalMove(double duty, double L);
+
+  // 上下駆動の回転数から距離への変換
+  double revToLength(int revolution);
 
 public:
   explicit ProbeSequence(DrillMotor *drillMotor, DCMotor *verticalMotor, Stepper *loadingMotor, QEI *verticalEncoder);
@@ -50,4 +103,6 @@ public:
   void start(ProbeNumber probeNumber);
 
   void stop();
+
+  ProbeSequenceState state();
 };
