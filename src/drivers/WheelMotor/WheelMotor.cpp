d35284a2f46edaf37061fa07a5537de20ec12ca0
ozkeeeeki <87401269+ozeeeeki@users.noreply.github.com>
2021-08-31 21:20:35 +0900

    merge WheelMotor

--- src/drivers/WheelMotor/WheelMotor.cpp
+++ src/drivers/WheelMotor/WheelMotor.cpp
@@ -1,41 +1,46 @@
 #include "WheelMotor.h"
 
-
-
-WheelMotor::WheelMotor(PwmOut *in1, PwmOut *in2)
-    : DCMotor(in1, in2, WHEEL_MOTOR_PWM_PERIOD), //
-      _dir(0)                                    //
-{}
+WheelMotor::WheelMotor(PwmOut *in1, PwmOut *in2) : DCMotor(in1, in2, WHEEL_MOTOR_PWM_PERIOD), _direction(STOP) {
+  if (_in2 != nullptr) {
+    *_in2 = 0.0;
+    _in2->suspend();
+  }
+  if (_in1 != nullptr) {
+    _in1->period_us(_periodUs);
+    *_in1 = 0.0;
+  }
+}
 
 void WheelMotor::forward(float duty) {
-  if (_dir != 1) {
-    if (_in2 != nullptr && _dir == -1) {
+  if (_direction == FOWARD) {
+    if (_in1 != nullptr) *_in1 = duty;
+  } else {
+    if (_in2 != nullptr) {
       *_in2 = 0.0;
-
       _in2->suspend();
     }
-
     if (_in1 != nullptr) {
       _in1->resume();
-
       _in1->period_us(_periodUs);
       *_in1 = duty;
     }
+  }
 }
 
 void WheelMotor::reverse(float duty) {
-  if (_dir != -1) {
-    if (_in1 != nullptr && _dir == 1) {
+  if (_direction == REVERSE) {
+    if (_in2 != nullptr) *_in2 = duty;
+  } else {
+    if (_in1 != nullptr) {
       *_in1 = 0.0;
-
       _in1->suspend();
     }
-
     if (_in2 != nullptr) {
       _in2->resume();
 
       _in2->period_us(_periodUs);
-      *_in2 = duty;
+      *_in1 = duty;
+    }
   }
 }
 
@@ -45,13 +50,9 @@ void WheelMotor::stop() {
   }
   if (_in1 != nullptr) {
     *_in1 = 0.0;
-
-      _in1->suspend();
   }
 
   if (_in2 != nullptr) {
     *_in2 = 0.0;
-
-      _in2->suspend();
   }
 }
