ade9a6a31b1297d02992d89683c50c378ea8ed93
ozkeeeeki <87401269+ozeeeeki@users.noreply.github.com>
2021-09-02 15:38:46 +0900

    modify:stabilizeクラスにスタビライザー展開処理を実装、フォーマット調整

--- src/middlewares/Stabilize/stabilize.cpp
+++ src/middlewares/Stabilize/stabilize.cpp
@@ -1,20 +1,14 @@
 #include "stabilize.h"
-#include "lsm9ds1.h"
 #include "WheelControl.h"
+#include "lsm9ds1.h"
 #include <math.h>
 
-Stabilize::Stabilize(LSM9DS1* imu, WheelMotor* leftWheelMotor, WheelMotor* rightWheelMotor):
-_imu(imu),
-_leftWheelMotor(leftWheelMotor),
-_rightWheelMotor(rightWheelMotor),
-_thread()
-{
-
-}
+Stabilize::Stabilize(LSM9DS1 *imu, WheelMotor *leftWheelMotor, WheelMotor *rightWheelMotor)
+    : _imu(imu), _leftWheelMotor(leftWheelMotor), _rightWheelMotor(rightWheelMotor), _thread() {}
 
 void Stabilize::start() {
-    _thread = make_unique<Thread>(STABILIZE_THREAD_PRIORITY, STABILIZE_THREAD_STACK_SIZE, nullptr, STABILIZE_THREAD_NAME);
-    _thread->start(callback(this, &Stabilize::threadLoop));
+  _thread = make_unique<Thread>(STABILIZE_THREAD_PRIORITY, STABILIZE_THREAD_STACK_SIZE, nullptr, STABILIZE_THREAD_NAME);
+  _thread->start(callback(this, &Stabilize::threadLoop));
 }
 
 void Stabilize::stop() {
@@ -22,31 +16,65 @@ void Stabilize::stop() {
   _thread.reset();
 }
 
-void Stabilize::threadLoop(){
-    while(true){
-        _theta = getTheta(_imu->accX(), _imu->accY(), _imu->accZ());
-        double diff = _theta;
-        _integral += diff * chrono::duration<float>(STABILIZE_PERIOD).count();;
-        _output = diff * _pGain + _integral * _iGain;
-        if(_output > 0.0){
-            _leftWheelMotor->reverse(_output);
-            _rightWheelMotor->reverse(_output);
-        }else{
-            _leftWheelMotor->forward(-1 * _output);
-            _rightWheelMotor->forward(-1 * _output);
-        }
-        ThisThread::sleep_for(STABILIZE_PERIOD);
+void Stabilize::threadLoop() {
+  invokeStabilizer();
+  waitingStabilizerOpening();
+  invokeAntiTolque();
+  while (true) {
+    _theta = getTheta(_imu->accX(), _imu->accY(), _imu->accZ());
+    double diff = _theta;
+    _integral += diff * chrono::duration<float>(STABILIZE_PERIOD).count();
+    ;
+    _output = diff * _pGain + _integral * _iGain;
+    // if (_output > 0.6) _output = 0.6;
+    // if(_output < -0.6)_output = -0.6;
+    if (_output > 0.0) {
+      _leftWheelMotor->reverse(_output);
+      _rightWheelMotor->reverse(_output);
+    } else {
+      _leftWheelMotor->forward(-1 * _output);
+      _rightWheelMotor->forward(-1 * _output);
     }
+    ThisThread::sleep_for(STABILIZE_PERIOD);
+  }
 }
 
-double Stabilize::getTheta(double accX, double accY, double accZ){
-    return  -1 * atan2(-accX, accZ); // 走行EMの座標系で調整
+double Stabilize::getTheta(double accX, double accY, double accZ) {
+  // return  -1 * atan2(-accX, accZ); // 走行EMの座標系で調整
+  return atan2(accZ, -accY); // FMの座標系で調整
 }
 
-double Stabilize::currentOutput(){
-    return _output;
+double Stabilize::currentOutput() {
+  return _output;
 }
 
-double Stabilize::currentTheta(){
-    return _theta;
+double Stabilize::currentTheta() {
+  return _theta;
+}
+
+void Stabilize::invokeStabilizer() {
+  _state = INVOKE_STABILIZER;
+  _leftWheelMotor->forward(0.6);
+  _rightWheelMotor->forward(0.6);
+  ThisThread::sleep_for(700ms);
+}
+
+void Stabilize::waitingStabilizerOpening() {
+  _state = WAITING_STABILIZER_OPENING;
+  _leftWheelMotor->forward(0.0);
+  _rightWheelMotor->forward(0.0);
+  ThisThread::sleep_for(500ms);
+}
+
+void Stabilize::invokeAntiTolque() {
+  _state = PREPARING_INVOKE_ANTI_TOLQUE;
+  _leftWheelMotor->forward(1);
+  _rightWheelMotor->forward(1);
+  ThisThread::sleep_for(1s);
+  _state = INVOKE_ANTI_TOLQUE;
+  _leftWheelMotor->reverse(0.6);
+  _rightWheelMotor->reverse(0.6);
+  ThisThread::sleep_for(100ms);
+  _leftWheelMotor->forward(0);
+  _rightWheelMotor->forward(0);
 }
--- src/middlewares/Stabilize/stabilize.h
+++ src/middlewares/Stabilize/stabilize.h
@@ -12,24 +12,40 @@
 
 #define STABILIZE_PERIOD 1ms
 
-class Stabilize{
+class Stabilize {
 public:
-    explicit Stabilize(LSM9DS1* imu, WheelMotor* leftWheelMotor, WheelMotor* rightWheelMotor);
-    void start();
-    void stop();
-    double currentTheta();
-    double currentOutput();
+  enum STABILIZE_STATE {
+    WAITING,
+    INVOKE_STABILIZER,
+    WAITING_STABILIZER_OPENING,
+    PREPARING_INVOKE_ANTI_TOLQUE,
+    INVOKE_ANTI_TOLQUE,
+    CALM_STABILIZE,
+    COMPLETE_STABILIZE
+  };
+  explicit Stabilize(LSM9DS1 *imu, WheelMotor *leftWheelMotor, WheelMotor *rightWheelMotor);
+  void start();
+  void stop();
+  double currentTheta();
+  double currentOutput();
+  STABILIZE_STATE state();
+
 private:
-    void threadLoop();
-    double getTheta(double accX, double accY, double accZ);
-    const double _pGain = 1.0;
-    const double _iGain = 0;
-    const double _eps = 0.08; // 5度程度
-    double _integral = 0.0;
-    double _output = 0.0;
-    double _theta = 0.0;
-    WheelMotor* _leftWheelMotor;
-    WheelMotor* _rightWheelMotor;
-    LSM9DS1* _imu;
-    unique_ptr<Thread> _thread;
+  void threadLoop();
+  void invokeStabilizer();
+  void waitingStabilizerOpening();
+  void invokeAntiTolque();
+  void checkCompleteStabilize();
+  double getTheta(double accX, double accY, double accZ);
+  const double _pGain = 1.0;
+  const double _iGain = 0;
+  const double _eps = 0.08; // 5度程度
+  double _integral = 0.0;
+  double _output = 0.0;
+  double _theta = 0.0;
+  STABILIZE_STATE _state;
+  WheelMotor *_leftWheelMotor;
+  WheelMotor *_rightWheelMotor;
+  LSM9DS1 *_imu;
+  unique_ptr<Thread> _thread;
 };
