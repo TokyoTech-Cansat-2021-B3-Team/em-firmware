48e746594520397332ff4a335fbd31d557add9f5
ozeeeeki <87401269+ozeeeeki@users.noreply.github.com>
2021-10-10 14:00:21 +0900

    modify: webserialplotterのフォーマットに合うようにprint内容を変更

--- src/main.cpp
+++ src/main.cpp
@@ -90,14 +90,15 @@ void printThreadLoop() {
   snprintf(printBuffer, PRINT_BUFFER_SIZE,
            "stts Ltsp Rtsp Lcsp Rcsp w_wh w_gy t_kf w_kf x_kf y_kf v_kf t_sm x_sm y_sm'\r\n");
   // snprintf(printBuffer, PRINT_BUFFER_SIZE, "stat Ltsp Rtsp Lcsp Rcsp t_kf x_kf y_kf\r\n");
-  bufferedSerial.write(printBuffer, strlen(printBuffer));
+  //bufferedSerial.write(printBuffer, strlen(printBuffer));
   while (true) {
 
-    snprintf(printBuffer, PRINT_BUFFER_SIZE, "$%d %f %f %f %f %f %f %f %f %f %f %f %f %f %f;\r\n",
-             runningSequence.state(), navi.leftTargetSpeed(), navi.rightTargetSpeed(), leftMotorSpeed.currentSpeedRPM(),
-             rightMotorSpeed.currentSpeedRPM(), localization.getAngularVelocityFromWheelOdometry(), -imu.gyrY(),
-             localization.theta(), localization.omega(), localization.x(), localization.y(), localization.v(),
-             simpleLocalization.theta(), simpleLocalization.x(), simpleLocalization.y());
+    double tmp = imu.gyrZ() * 3.141592653589793 / 180.0;
+    snprintf(printBuffer, PRINT_BUFFER_SIZE,
+             "Lcsp:%f, Rcsp:%f, w_wh:%f, w_gy:%f, t_kf:%f, w_kf:%f, x_kf:%f, y_kf:%f, v_kf:%f, slip:%f, beta:%f\r\n",
+             leftMotorSpeed.currentSpeedRPM(), rightMotorSpeed.currentSpeedRPM(),
+             localization.getAngularVelocityFromWheelOdometry(), tmp, localization.theta(), localization.omega(),
+             localization.x(), localization.y(), localization.v(), localization.slip(), localization.beta());
 
     // snprintf(printBuffer, PRINT_BUFFER_SIZE, "%f %f %f \r\n", localization.x(),
     // localization.y(),sqrt(localization.x()*localization.x()+localization.y()*localization.y()));
